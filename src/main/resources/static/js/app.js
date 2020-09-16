ee7eede063dbc7353195e8ace9b98aa1fad9eb91
Peter Thongpasouk <thongpasoukpeter@gmail.com>
2020-09-15 23:17:57 -0400

    center h3 title and begining to load beers from api request to the dom via js

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,4 +1,5 @@
 const BASE_API = 'http://localhost:8080/beerings'
+const sugs = document.getElementById('suggestions');
 document.addEventListener('DOMContentLoaded', () =>{
 
     document.getElementById('food_input').addEventListener('click', function(){
@@ -8,14 +9,19 @@ document.addEventListener('DOMContentLoaded', () =>{
 });
 
 function getPairings(){
-    var foods = document.getElementById('food_input').value;
+    let foods = document.getElementById('food_input').value;
     console.log(foods)
-    var splitFoods = foods.split(" ");
+    let splitFoods = foods.split(" ");
     fetch(BASE_API + '/food?foods=' + splitFoods.join("_"))
        .then(response => response.json())
        .then(data => {
+        data.array.forEach(element => {
+            
+        });
+        
+
 
        })
-//       .then(data => console.log(data));
+    //   .then(data => console.log(data));
 
 }
